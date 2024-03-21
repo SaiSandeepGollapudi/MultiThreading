39d5dc1fa8e34023f44d30dc57206afb2be95231
SaiSandeepGollapudi <gsandeep2406@gmail.com>
2024-03-21 14:35:49 -0400

    first commit

---
+++ src/main/java/org/example/RunnableThreadTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+class MyRunnable implements Runnable
+{
+    public void run()
+    {
+        int i=1;    
+        while(true)
+        {
+            System.out.println(i+"Hello");
+            i++;
+        }
+    }
+}
+
+public class RunnableThreadTest
+{
+
+    public static void main(String[] args) {
+
+
+        MyRunnable t = new MyRunnable();
+        Thread th = new Thread(t);
+
+        th.start();
+
+        int i = 1;
+        while (true) {
+            System.out.println(i + "World");
+        }
+
+    }
+}
